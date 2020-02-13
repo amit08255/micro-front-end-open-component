a62b277a41558911e991bf5851e2266e0e64a65f
nidhinkumar <nidhinkumar@codingtown.com>
2020-02-13 19:24:03 +0530

    add open component

---
+++ components/users/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./styles.css";
+
+const App = props =>
+  <div className={styles.special}>
+    <h1>
+      This is a user component
+    </h1>
+  </div>;
+
+export default App;
